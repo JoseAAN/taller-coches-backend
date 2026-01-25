7e9e111d67b28d28a9d1fa252de947a28f44f772
AdexeMonzon <adexems.projects@gmail.com>
2026-01-25 09:35:19 +0000

    fix/ Minor fixes to CORS and validation issues for frontend consumption

--- backend/app/interfaces/Sorter.php
+++ backend/app/interfaces/Sorter.php
@@ -4,8 +4,6 @@ namespace App\interfaces;
 
 use Illuminate\Http\Request;
 
-
-
 interface Sorter
 {
     public function sort($query, Request $request, array $allowedSorts, string $defaultSort = 'created_at', string $defaultOrder = 'desc');
