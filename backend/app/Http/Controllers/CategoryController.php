ed3c6b4b2692d0f8db365441470fee9e27111d6a
AdexeMonzon <adexems.projects@gmail.com>
2026-03-23 19:25:32 +0000

    minor fixed

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -23,7 +23,7 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $data = $request->validate([
-            'name' => 'required/string/max:255',
+            'name' => 'required|string|max:255',
         ]);
         $category = Category::create($data);
         return response()->json($category, 201);
